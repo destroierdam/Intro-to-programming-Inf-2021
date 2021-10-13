ba9794d4feb9d82b78b46d2fa63a4451d89af8d3
Damian Boytchev <dam-bb@hotmail.com>
2021-10-13 14:16:58 +0300

    Add code from Lesson 1.

---
+++ Week01/main.cpp
@@ -0,0 +1,19 @@
+// Week1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Пенчо
+#include <iostream>
+int main() {
+    int salary = 123;
+    float massInKg = 10.5;
+    // std::cout << "Enter the mass: ";
+    // std::cin >> massInKg;
+
+
+    std::cout << "-----" << 12 << "-------";
+
+    std::cout << salary;
+    std::cout << "\n";
+    std::cout << massInKg;
+    std::cout << "\n";
+    std::cout << "Hello World!\n";
+    std::cout << "That is mass " << massInKg;
+}
