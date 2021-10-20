b0ce9badad618a91c2e1213dd366c9c8e0a91f01
destroierdam <33325102+destroierdam@users.noreply.github.com>
2021-10-20 21:01:11 +0300

    Add code from week 2

---
+++ Week02/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+int main() {
+	const float pi = 3.14;
+	float firstNumber;
+	std::cin >> firstNumber;
+	firstNumber /= pi;
+
+	std::cout << "Multiplied by pi: ";
+	std::cout << firstNumber;
+  
+	// Sbor na dve chisla
+	
+	std::cout << "Enter your first number.";
+	int firstNumber;
+	std::cin >> firstNumber;
+	std::cout << "Enter your second number.";
+	int secondNumber;
+	std::cin >> secondNumber;
+	
+	std::cout << firstNumber << "\n";
+	
+}
